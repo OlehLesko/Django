0cd91f6659eedfae43416fd76ebc8b500ffca038
oleh <olehlesko81@gmail.com>
2021-12-15 15:08:34 +0200

    I extracted the data from the parsing and added it to the json file

--- test1/main/static/main/js/script_Asia_window.js
+++ test1/main/static/main/js/script_Asia_window.js
@@ -1,3 +1,13 @@
 function myFunction() {
-    document.getElementById('text_field').innerHTML = "Hello, world";
+    const data = require('./file.json');
+    console.log(data);
+//    var json = require('../templates/main/file.json'); //(with path)
+//    console.log(json);
+//    import * as data from './file.json';
+//////    import {asia_function().json_dictionary} from "../templates/main/asia.py";
+//    for(let [key, value] of data) {
+//        console.log(key, value);
+//    }
+//    console.log(data);
+////    document.getElementById('text_field').innerHTML = "Hello, world";
 }
---
+++ test1/main/templates/main/asia.py
@@ -0,0 +1,40 @@
+from config_file import *
+from request_module import request
+from get_general_vaccinated_data_asia import total_vaccinated
+import json
+
+def asia_function():
+    headers = headers_asia
+    html = request(url_asia, headers)
+
+    all_infected = html.find("td", class_="bg-total larger").text
+    deaths = html.find_all("td", class_="bg-total")[3].text
+    recovered = html.find_all("td", class_="bg-total")[5].text
+    now_ill = html.find_all("td", class_="bg-total")[7].text
+    total_vaccinated_people = total_vaccinated()
+    # print(f"\n{all_infected}\n\n {deaths} \n {recovered} \n {now_ill} \n {total_vaccinated_people}")
+    # info_text.insert(1.0, f"\n{all_infected}\n\n {deaths} \n {recovered} \n {now_ill} \n {total_vaccinated_people}")
+    # info_text['state'] = 'disabled'
+
+    dict = {}
+    dict['Infected'] = all_infected
+    dict['Deaths'] = deaths
+    dict['Recovered'] = recovered
+    dict['Now ill'] = now_ill
+    dict['Total vaccinated people'] = total_vaccinated_people
+
+
+
+    # for key,value in dict.items():
+    #     print(key, value)
+    with open("file.json", "w") as out:
+        # out = json.dumps(dict)
+        out.write(json.dumps(dict))
+
+    json_dictionary = json.dumps(dict)
+
+    print(json_dictionary)
+
+
+if __name__ == "__main__":
+    asia_function()
